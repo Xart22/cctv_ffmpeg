328e727895782a6c611fc95a050c54a582d74499
xart22 <fajar.rn22@gmail.com>
2022-06-19 20:02:00 +0700

    change scale cctv

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { exec } = require("child_process");
 const PORT = 4000;
 
 exec(
-  `ffmpeg -i rtsp://admin:hik12345@118.96.244.84:554/Streaming/Channels/101/ -fflags flush_packets -max_delay 5 -flags -global_header -hls_time 5 -hls_list_size 3 -vcodec copy -y 'file location'`,
+  `ffmpeg -i rtsp://admin:hik12345@118.96.244.84:554/Streaming/Channels/101/ -fflags flush_packets -max_delay 5 -flags -global_header -hls_time 5 -hls_list_size 3 scale=320:-1 -vcodec copy -y 'file location'`,
   (err, stdout, stderr) => {
     if (err) {
       console.error(`exec error: ${err}`);
