481c40836669c833a1ab8d27ff6b3d0b402fe112
xart22 <fajar.rn22@gmail.com>
2022-06-20 19:31:07 +0700

    fixing all

--- gps.js
+++ gps.js
@@ -1,51 +1,27 @@
+const { SerialPort } = require("serialport");
+const { ReadlineParser } = require("@serialport/parser-readline");
+const GPS = require("gps");
+const port = new SerialPort({ path: "COM6", baudRate: 9600 });
 const express = require("express");
 const app = express();
-const { SerialPort } = require("serialport");
+const PORT = 3000;
 
-var port = 3000;
+const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
 
-var arduinoCOMPort = "COM6";
+const gps = new GPS();
 
-const arduinoSerialPort = new SerialPort({
-  path: arduinoCOMPort,
-  baudRate: 9600,
+gps.on("data", function (data) {
+  console.log(data);
 });
 
-arduinoSerialPort.on("open", function () {
-  console.log("Serial Port " + arduinoCOMPort + " is opened.");
-
-  arduinoSerialPort.on("data", function (data) {
-    console.log(data.toString());
-  });
-
-  arduinoSerialPort.on("close", function () {
-    console.log("close");
-  });
-
-  arduinoSerialPort.read(function (err, data) {
-    console.log(data.toString());
-  });
+parser.on("data", function (data) {
+  gps.update(data);
 });
 
-app.get("/", function (req, res) {
-  return res.send("Working");
+app.get("/location", (req, res) => {
+  res.send(gps.state);
 });
 
-app.get("/:action", function (req, res) {
-  var action = req.params.action;
-
-  if (action == "led") {
-    arduinoSerialPort.write("w");
-    return res.send("Led light is on!");
-  }
-  if (action == "off") {
-    arduinoSerialPort.write("t");
-    return res.send("Led light is off!");
-  }
-
-  return res.send("Action: " + action);
-});
-
-app.listen(port, function () {
-  console.log("Example app listening on port http://0.0.0.0:" + port + "!");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const { exec } = require("child_process");
-
-const PORT = 4000;
+const { SerialPort } = require("serialport");
+const { ReadlineParser } = require("@serialport/parser-readline");
+const GPS = require("gps");
+const port = new SerialPort({ path: "COM6", baudRate: 9600 });
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
 exec(
   `ffmpeg -i rtsp://admin:hik12345@118.96.244.84:554/Streaming/Channels/101/ -fflags flush_packets -max_delay 5 -flags -global_header -hls_time 5 -hls_list_size 3 scale=320:-1 -vcodec copy -y 'file location'`,
@@ -23,3 +28,23 @@ setInterval(() => {
   });
   console.log(result);
 }, 5000);
+
+const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
+
+const gps = new GPS();
+
+gps.on("data", function (data) {
+  console.log(data);
+});
+
+parser.on("data", function (data) {
+  gps.update(data);
+});
+
+app.get("/location", (req, res) => {
+  res.send(gps.state);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
